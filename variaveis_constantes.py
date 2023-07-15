ca33b0a7ad9310f98d72b60b3e3de304199a82d7
Larissa <larissa.santana5@fatec.sp.gov.br>
2023-07-15 20:28:50 -0300

    praticas da aula

---
+++ variaveis_constantes.py
@@ -0,0 +1,12 @@
+nome = "Fulano"
+idade = 25
+
+nome, idade = "Beltrano", 43
+
+print(nome, idade)
+
+limite_saque_diario = 1000
+
+BRAZILIAN_STATES = ["SP", "RJ", "SC", "RS"]
+
+print(BRAZILIAN_STATES)
