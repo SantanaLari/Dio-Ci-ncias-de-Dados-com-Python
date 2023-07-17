261538ae671ddb1c396664e4ac96666da5d795bf
Larissa <larissa.santana5@fatec.sp.gov.br>
2023-07-17 18:01:51 -0300

    operadores aritmeticos

---
+++ operadores_aritmeticos.py
@@ -0,0 +1,22 @@
+produto_1 = 20
+produto_2 = 10
+
+print(produto_1 + produto_2)
+print(produto_1 - produto_2)
+print(produto_1 / produto_2)
+print(produto_1 // produto_2)
+print(produto_1 * produto_2)
+print(produto_1 % produto_2)
+print(produto_1 ** produto_2)
+
+x1 = 10 + 5 * 4 
+print(x1)
+
+x2 = (10 + 5) * 4 
+print(x2)
+
+y1 = (10 / 2) + (25 * 2) - (2 ** 2)
+print(y1)
+
+y2 = (10 / 2) + 25 * ((2 - 2) ** 2)
+print(y2)
